d2b45189ed67bd1df492909acb353527ac2c43f7
Evan Altshule <ealtshulte@cs.ucsb.edu>
2019-05-26 12:38:20 -0700

    with getters

--- Post.java
+++ Post.java
@@ -7,6 +7,7 @@ public class Post
 	private User user;
 	private int parentID;
 
+
 	public Post(ArrayList<String> tags, String message, User user, int parentID)
 	{
 		this.tags = tags;
@@ -14,4 +15,22 @@ public class Post
 		this.user = user;
 		this.parentID = parentID;
 	}
-}
+
+	public ArrayList<String> getTags(){
+		return tags;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public User getUser(){
+		return user;
+	}
+
+	public int getParentID(){
+		return parentID;
+	}
+
+
+
