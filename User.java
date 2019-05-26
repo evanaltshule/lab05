03fbf84f8f241cf712a8f2c7c124a1805a4d4ebb
George Kripac <georgekripac@umail.ucsb.edu>
2019-05-26 13:10:26 -0700

    update user

--- User.java
+++ User.java
@@ -1,7 +1,21 @@
 //Evan Altshule and George Kripac
 
+import java.util.*;
+
 public class User implements Observer
 {
+	private static int users = 1;
+	private int userID;
+	private ArrayList<String> subscribed;
+	private ArrayList<Post> posts;
+
 	public User()
-	{}
+	{
+		userID = users;
+		users++;
+	}
+
+	public int getUserID{
+		return userID;
+	}
 }
