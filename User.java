e7feb09a5f3dd7402c7fc6acce1c483af083981d
George Kripac <georgekripac@umail.ucsb.edu>
2019-05-31 18:38:37 -0700

    user getters

--- User.java
+++ User.java
@@ -6,8 +6,8 @@ public class User implements Observer
 {
 	private static int users = 1;
 	private int userID;
-	private ArrayList<String> subscribed;
-	private ArrayList<Post> posts;
+	private ArrayList<String> subscribed = new ArrayList<String>();
+	private ArrayList<Post> posts = new ArrayList<Post>();
 
 	public User()
 	{
@@ -20,10 +20,21 @@ public class User implements Observer
 		return userID;
 	}
 
+	public ArrayList<String> subscribed()
+	{
+		return subscribed;
+	}
+
+	public ArrayList<Post> getPosts()
+	{
+		return posts;
+	}
+
 	@Override
 	public void update(Post p)
 	{
 		System.out.println("***** Updating UserID: " + userID + " *****");
 		p.print();
+		System.out.println("******************************");
 	}
 }
